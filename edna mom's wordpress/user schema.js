a5bd1cc71ee54276a87a0de38d37180f55320dcd
youngT2 <166722946+youngT2@users.noreply.github.com>
2024-05-11 11:17:42 -0400

    Delete edna mom's wordpress directory

--- edna mom's wordpress/user schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'editor'], default: 'editor' }
-});
-
-module.exports = mongoose.model('User', UserSchema);
